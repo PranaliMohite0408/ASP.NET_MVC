f294951eaece9fbc251e7be502adaf0ee02699fe
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-04-06 11:14:05 +0530

    Action Method in ASP.NET MVC

---
+++ Action_Method_Demo/Action_Method_Demo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Action_Method_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Show()
+        {
+            return "This is a Second action method of home controller";
+        }
+
+        public ActionResult AboutUS()
+        {
+            return View();
+        }
+        public int StudentID(int id)
+        {
+            return id;
+        }
+    }
+}
---
+++ Action_Method_Demo/Action_Method_Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Action_Method_Demo.MvcApplication" Language="C#" %>
