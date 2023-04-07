e1ed5ed503de321438303450bb9f33c82e529ec0
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-04-07 17:54:06 +0530

    View Data in ASP.NET MVC

---
+++ View_Data_Demo/View_Data_Demo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using View_Data_Demo.Models;
+
+namespace View_Data_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Message from View Data !!";
+            ViewData["CurrentTime"] = DateTime.Now.ToLongTimeString();
+
+            string[] Fruits = {"Apple","Banana", "Orange", "Mango"};
+            ViewData["FruitsArray"] = Fruits;
+
+            ViewData["SportsList"] = new List<String>()
+            {
+                "Cricket",
+                "Hockey",
+                "Footboll",
+                "Kho-Kho"
+            };
+
+            Employee John = new Employee();
+            John.EmpID = 10;
+            John.EmpName = "John Sen";
+            John.EmpDesignation = "Manager";
+
+            ViewData["Employee"] = John;
+            
+            return View();
+        }
+    }
+}
---
+++ View_Data_Demo/View_Data_Demo/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace View_Data_Demo.Models
+{
+    public class Employee
+    {
+        public int EmpID{ get; set; }
+        public string EmpName { get; set; }
+
+        public string EmpDesignation{ get; set; }
+    }
+
+}
