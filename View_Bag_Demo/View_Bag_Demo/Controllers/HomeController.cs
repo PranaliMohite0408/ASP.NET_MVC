3c460f96d6ed0356486d723ac383647225946bd2
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-04-08 14:49:57 +0530

    View Bag in ASP.NET MVC

---
+++ View_Bag_Demo/View_Bag_Demo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using View_Bag_Demo.Models;
+
+namespace View_Bag_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Message From ViewBag !!";
+            ViewBag.CurrentDate = DateTime.Now.ToLongDateString();
+
+            string[] fruits = { "Apple", "Mango", "Orange", "Banana" };
+            ViewBag.FruitsArray = fruits;
+
+            ViewBag.SportsList = new List<string>()
+            {
+                   "Cricket",
+                   "Footboll",
+                   "Baseball",
+                   "Hockey"
+            };
+
+            Employee obj = new Employee();
+
+            obj.EmpID = 11;
+            obj.EmpName = "Jay Ganesh";
+            obj.EmpDesignation = "Software Developer";
+
+            ViewBag.Employee = obj;
+
+            //ViewBag.CommonMessage = "This Message is accessible by ViewBag and ViewData ";
+
+            ViewData["CommonMessage"] = "This Message is accessible by ViewBag and ViewData ";
+
+            return View();
+        }
+    }
+}
---
+++ View_Bag_Demo/View_Bag_Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="View_Bag_Demo.MvcApplication" Language="C#" %>
