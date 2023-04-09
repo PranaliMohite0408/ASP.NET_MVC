731dc38e2f00f4fe3d3bcafb79d2bacff25099b4
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-04-09 09:38:28 +0530

    Temp Data in ASP.NET MVC

---
+++ TempData_Demo/TempData_Demo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TempData_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Var1"] = "Message from View Data";
+            ViewBag.Var2 = "Message form View Bag";
+
+            TempData["Var3"] = "Message from Temp Data";
+
+            //string[] Games = {"Cricket", "Baseball", "Hockey"};
+            //TempData["GamesArray"] = Games;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if(TempData["Var3"] != null)
+            {
+                TempData["Var3"].ToString();
+            }
+            return View();
+        }
+    }
+}
