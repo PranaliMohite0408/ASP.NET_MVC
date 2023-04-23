136ca5665c5500d2b7ca2f9c90206e059cd974a3
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-04-23 18:57:52 +0530

    Strongly Typed Partial View

---
+++ Strongly_Typed_PartialView/Strongly_Typed_PartialView/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Strongly_Typed_PartialView.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Strongly_Typed_PartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        List<Product> ProductsList = new List<Product>()
+        {
+            new Product {Id = 1, Name = "Reebok Shoes", Price=1000, Picture = "~/Images/IMG1.jfif"},      
+            new Product {Id = 2, Name = "Sparks", Price=2000, Picture = "~/Images/IMG2.jfif"},
+            new Product {Id = 3, Name = "Reebok Shoes", Price=3000, Picture = "~/Images/IMG3.jfif"}
+        };
+
+        public ActionResult Index()
+        {         
+            return View(ProductsList);
+        }
+    }
+}
---
+++ Strongly_Typed_PartialView/Strongly_Typed_PartialView/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Strongly_Typed_PartialView.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Picture { get; set; }
+
+    }
+}
