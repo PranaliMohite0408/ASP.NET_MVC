770d740f0015d5af6b72ad4a14fd3f48439791d3
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-04-26 14:49:43 +0530

    Data Annotations in ASP.NET MVC

---
+++ Data_Annotations/Data_Annotations/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Data_Annotations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Data_Annotations.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Employee obj)
+        {
+            if(ModelState.IsValid == true)
+            {
+                ViewData["SuccessMessage"] = "<script>alert('Data Has been submited !!!')</script>";
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
---
+++ Data_Annotations/Data_Annotations/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Data_Annotations.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "ID is Mandatory")]
+        public int EmployeeId{ get; set; }
+
+        [Required(ErrorMessage = "Name is Mandatory")]
+        public string EmployeeName { get; set; }
+
+        [Required(ErrorMessage = "Age is Mandatory")]
+        public int? EmployeeAge { get; set; }
+
+        [Required(ErrorMessage = "Gender is Mandatory")]
+        public string EmployeeGender { get; set; }
+        
+        [Required(ErrorMessage = "Email is Mandatory")]
+        public string EmplpyeeEmail{ get; set; }
+
+    }
+}
