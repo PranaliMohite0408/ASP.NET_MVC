429561b7d22e94b482d206cf7eac955cf64606e2
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-05-22 15:13:23 +0530

    Filter Concept in MVC

---
+++ ExceptionFilterMVC/ExceptionFilterMVC/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ExceptionFilterMVC.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        // GET: Home
+       
+        public ActionResult Index()
+        {
+            throw new Exception();
+            return View();
+        }
+      
+        public ActionResult About()
+        {
+            throw new Exception();
+            return View();
+        }
+    }
+}
